ec52b433e62ae891fd7f04fec491f812cb848790
Alex Boisvert <alex.boisvert@gmail.com>
2013-12-09 12:19:01 -0800

    Remove ConfigReader

--- src/main/scala/chompdb/server/Config.scala
+++ src/main/scala/chompdb/server/Config.scala
@@ -5,10 +5,3 @@ import f1lesystem.FileSystem
 trait Config {
   // val domain: DomainInfo
 }
-
-trait ConfigReader {
-  val fs: FileSystem
-
-  def read(path: FileSystem#Path): Config
-  def write(path: FileSystem#Path, config: Config): Unit
-}
