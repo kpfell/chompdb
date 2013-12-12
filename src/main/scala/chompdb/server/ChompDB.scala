862cdfc2d0988719829c855f43808f98386155d7
Katherine Fellows <k@kpf.me>
2013-12-11 18:35:25 -0800

    first draft of getNewVersionNumber and downloadDatabaseVersion

--- src/main/scala/chompdb/server/ChompDB.scala
+++ src/main/scala/chompdb/server/ChompDB.scala
@@ -1,7 +1,9 @@
 package chompdb.server
 
 import chompdb.Database
+import chompdb.store.ShardedWriter
 import f1lesystem.FileSystem
+import scala.collection.mutable.SynchronizedSet
 
 class ChompDB(
 	val databases: Seq[Database],
@@ -13,6 +15,7 @@ class ChompDB(
 	val fs: FileSystem,
 	val rootDir: FileSystem#Dir
 ) {
+	/* Returns version number, if any, of the latest database version to download from S3. */
 	def getNewVersionNumber(database: Database): Option[Long] = {
 		database.versionedStore.mostRecentVersion match {
 			case Some(latestRemoteVersion) => {
@@ -29,8 +32,50 @@ class ChompDB(
 		}
 	}
 
-	// def downloadDatabaseVersion(version: Int) = {
-	// }
+	// Copies a database version from S3 to local filesystem. 
+	// NOTE: Assumes that the database directory already exists locally--just not one specific version.
+
+	// TODO: numThreads should not be hard set
+	def downloadDatabaseVersion(database: Database, version: Long) = {
+		val numThreads = 5
+
+		val remoteDir = database.versionedStore.versionPath(version)
+		val localDir = rootDir /+ database.name /+ version.toString
+		localDir.mkdir()
+
+		val writers = (0 until numThreads) map { i => 
+			new ShardedWriter {
+				val writers = numThreads
+				val writerIndex = i
+				val shardsTotal = remoteDir
+					.listFiles
+					.map(_.filename)
+					.filter(_.endsWith(".blob"))
+					.size
+
+				val baseDir = remoteDir
+			}
+		}		
+
+		copyShards(writers, localDir)
+
+		def copyShards(writers: Seq[ShardedWriter], versionDir: FileSystem#Dir) {
+			for (w <- writers) {
+				for (baseFile <- w.shardFiles) {
+					copy(baseFile.indexFile, versionDir / baseFile.indexFile.filename)
+					copy(baseFile.blobFile, versionDir / baseFile.blobFile.filename)
+				}
+			}
+		}
+
+		def copy(from: FileSystem#File, to: FileSystem#File) {
+			from.readAsReader { reader =>
+				to.write(reader, from.size)
+			}
+		}
+
+		// Need to succeedVersion...!
+	}
 
 	// def start() {
 	// 	executor.schedule(timerTask, period)
