009e87205dbb93bc52f211aaa8ef0e7c50b4e6ae
Katherine Fellows <k@kpf.me>
2014-02-13 11:27:33 -0800

    updated replicationFactor usage

--- core/src/main/scala/chompdb/server/HashRing.scala
+++ core/src/main/scala/chompdb/server/HashRing.scala
@@ -21,20 +21,20 @@ class HashRing(chomp: Chomp) {
       case 0 => throw new NodeNotFoundException("No nodes available on the hash ring.")
       case 1 => Set(nodeMap.get(nodeMap.firstKey))
       case _ => {
-        def assembleNodeSet(nodeSet: Set[Node], count: Int): Set[Node] = count match {
+        def assembleNodeSet(nodeSet: Set[Node], count: Int, baseHashValue: Int): Set[Node] = count match {
           case c if c < 0 => {
             throw new InvalidReplicationFactorException(s"Invalid replicationFactor provided: $c")
           }
           case 0 => nodeSet
           case c if c > 0 => {
-            val nearestNodeHashValue = Option(nodeMap.ceilingKey(shardHashValue))
+            val nearestNodeHashValue = Option(nodeMap.ceilingKey(baseHashValue))
               .getOrElse(nodeMap.firstKey)
 
-            assembleNodeSet(nodeSet + nodeMap.get(nearestNodeHashValue), c - 1)
+            assembleNodeSet(nodeSet + nodeMap.get(nearestNodeHashValue), c - 1, baseHashValue + 1)
           }
         }
 
-        assembleNodeSet(Set.empty[Node], chomp.replicationFactor)
+        assembleNodeSet(Set.empty[Node], chomp.replicationFactor, shardHashValue)
       }
     } 
   }
