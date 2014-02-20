db88449a9d17a272dc059a4c7b6bd8d0bf1ea6d6
Katherine Fellows <k@kpf.me>
2014-02-20 12:08:25 -0800

    added logger to utils

---
+++ core/src/main/scala/chompdb/util/Logger.scala
@@ -0,0 +1,27 @@
+package chompdb.util
+
+trait Logger {
+  def debug(message: String): Unit
+  def info(message: String): Unit
+  def warn(message: String): Unit
+  def error(message: String, t: Throwable): Unit
+}
+
+trait ConsoleLogger extends Logger {
+  override def debug(message: String) {
+    Console.println("[DEBUG] " + message)
+  }
+
+  override def info(message: String) {
+    Console.println("[DEBUG] " + message)
+  }
+
+  override def warn(message: String) {
+    Console.println("[WARN] " + message)
+  }
+
+  override def error(message: String, t: Throwable) {
+    Console.println("[ERROR] " + message)
+    t.printStackTrace()
+  }
+}
